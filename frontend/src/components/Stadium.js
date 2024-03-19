e56fbd92ceb949a3d49460fc6c77d0651822de59
seckin <skuvandik@gmail.com>
2024-03-19 23:17:19 +0300

    Fix dates

--- frontend/src/components/Stadium.js
+++ frontend/src/components/Stadium.js
@@ -7,11 +7,21 @@ import { MdDeleteForever, MdEdit } from 'react-icons/md';
 import { MdAddBox, MdModeEditOutline, MdRemoveCircleOutline } from 'react-icons/md';
 
 function Stadium({ stadium, onEdit, onDelete }) {
+
+    // Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+    const formatDate = (isoDateString) => {
+        return new Date(isoDateString).toLocaleDateString('en-US', {
+            month: '2-digit',
+            day: '2-digit',
+            year: 'numeric',
+        });
+    };
+
     return (
         <tr>
             <td>{stadium.name}</td>
             <td>{stadium.capacity}</td>
-            <td>{stadium.constructionDate}</td>
+            <td>{formatDate(stadium.constructionDate)}</td>
             <td><MdRemoveCircleOutline onClick={() => onDelete(stadium._id)} style={{cursor: 'pointer'}} /></td>
             <td><MdModeEditOutline onClick={() => onEdit(stadium)} style={{cursor: 'pointer'}} /></td>
         </tr>
